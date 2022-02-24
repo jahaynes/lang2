5e0f1958c40c64ec2ad5dda4376c8f4e3b3f4d88
John Haynes <john.andrew.haynes@gmail.com>
2022-02-24 09:36:33 +0100

    division test

--- compiler-lib/test/Parse/ParserTest.hs
+++ compiler-lib/test/Parse/ParserTest.hs
@@ -3,6 +3,7 @@
 
 module Parse.ParserTest (parserTests) where
 
+import           Data.ByteString             (ByteString)
 import qualified Data.ByteString.Char8 as C8
 import           Hedgehog
 import qualified Hedgehog.Gen as G
@@ -16,19 +17,20 @@ import Parse.ExpressionParser
 import Parse.Lexer
 import Parse.LexState
 import Parse.Parser
+import Parse.Token
 
 parserTests :: Group
 parserTests =
     Group "Parsing" [ ("Simple expressions are parsed", prop_simpleExpressionsAreParsed)
+                    , ("Division is left associative", prop_divisionLeftAssociative)
                     ]
 
 prop_simpleExpressionsAreParsed :: Property
 prop_simpleExpressionsAreParsed = property $ do
     num1 :: Integer <- forAll $ G.integral (linear 0 100)
     num2 :: Integer <- forAll $ G.integral (linear 0 100)
-    let input = C8.pack $ printf "%d + %d" num1 num2
-        Right (LexState _ strRemainder, tokens) = runLexer input
-    strRemainder === ""
+
+    let tokens = asTokens (printf "%d + %d" num1 num2)
     case runParser parseExpr tokens of
         Right (tokRemainder, (pos,EBinPrimOp AddI (EApp (ETerm (LitInt a)) []) (EApp (ETerm (LitInt b)) []))) -> do
             pos === SourcePos 1 1
@@ -37,6 +39,22 @@ prop_simpleExpressionsAreParsed = property $ do
             b === num2
         x -> error $ show x
 
+prop_divisionLeftAssociative :: Property
+prop_divisionLeftAssociative = property $ do
+    let tokens = asTokens "8 / 4 / 2"
+        Right ([], (SourcePos 1 1, expr)) = runParser parseExpr tokens
+    expr ===
+         EBinPrimOp DivI (EBinPrimOp DivI (EApp (ETerm (LitInt 8)) [])
+                                          (EApp (ETerm (LitInt 4)) []))
+                         (EApp (ETerm (LitInt 2)) [])
+    -- TODO reduce these (App x [])
+
 -- TODO
 _prop_negativeNumbersAreParsed :: Property
 _prop_negativeNumbersAreParsed = error "TODO"
+
+asTokens :: String -> [(SourcePos, Token ByteString)]
+asTokens input =
+    case runLexer $ C8.pack input of
+        Left e            -> error $ "Failed to tokenize: " <> e
+        Right (_, tokens) -> tokens
