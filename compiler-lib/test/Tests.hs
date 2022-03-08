d1b4bd955fc8c59e0f8710bf3279a859a049df79
John Haynes <john.andrew.haynes@gmail.com>
2022-03-08 11:25:53 +0100

    remove legacy

--- compiler-lib/test/Tests.hs
+++ compiler-lib/test/Tests.hs
@@ -1,13 +1,9 @@
 import Hedgehog (checkParallel)
 
-import Parse.LexerTest       (lexerTests)
-import Parse.ParserTest      (parserTests)
 import Parse2.ByteStringTest (byteStringTests)
 import Parse2.Lexer2Test     (lexer2Tests)
 
 main :: IO ()
 main = mapM_ checkParallel [ byteStringTests
-                           , lexerTests
                            , lexer2Tests
-                           , parserTests
                            ]
