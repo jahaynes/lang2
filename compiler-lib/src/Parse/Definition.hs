0a3e108d471989edf072bbe11c6cbf4415a1bafa
John Haynes <john.andrew.haynes@gmail.com>
2022-03-27 12:39:46 +0200

    fix data definition parsing

--- compiler-lib/src/Parse/Definition.hs
+++ compiler-lib/src/Parse/Definition.hs
@@ -11,6 +11,9 @@ import Parse.Token
 
 import Data.ByteString (ByteString)
 
+import           Data.Vector      ((!?))
+import qualified Data.IntSet as IS
+
 parseDefns :: Parser ParseState [Defn ByteString]
 parseDefns = many' parseDefn
 
@@ -33,21 +36,50 @@ parseFunDefn = do
             [] -> expr
             _  -> ELam vars expr
 
+atLineStart :: Parser ParseState a -> Parser ParseState a
+atLineStart p = Parser $ \ps ->
+    if isAtLineStart ps
+        then runParser p ps
+        else Left "Not at line start"
+
+isAtLineStart :: ParseState -> Bool
+isAtLineStart ps =
+    case ps_positions ps !? ps_pos ps of
+        Nothing -> False
+        Just x  -> x `IS.member` ps_lineStarts ps
+
+notAtLineStart :: Parser ParseState a -> Parser ParseState a
+notAtLineStart p =
+    Parser $ \ps ->
+        if isAtLineStart ps
+            then Left "At line start"
+            else runParser p ps
+
+notAtLineStarts :: Parser ParseState a -> Parser ParseState [a]
+notAtLineStarts p = Parser $ go []
+    where
+    go acc ps   
+        | isAtLineStart ps = Right (ps, reverse acc)
+        | otherwise =
+            case runParser p ps of
+                Left _ -> Right (ps, reverse acc)
+                Right (ps', x) -> go (x:acc) ps'
+
 parseDataDefn :: Parser ParseState (Defn ByteString)
 parseDataDefn = do
-    name         <- parseUpperStart
-    tyVars       <- parseWhileColumns MoreRight parseTypeVariable
-    _            <- token TEq
-    constructors <- (:) <$> parseConstructor
-                        <*> many' (token TPipe *> parseConstructor)
-    pure $ TypeDefn name tyVars constructors
+    name   <- atLineStart parseUpperStart
+    tyVars <- notAtLineStarts parseTypeVariable
+    _      <- notAtLineStart (token TEq)
+    dc1    <- notAtLineStart parseDataConstructor
+    dcs    <- many' (notAtLineStart (token TPipe) *> notAtLineStart parseDataConstructor)
+    pure $ TypeDefn name tyVars (dc1:dcs)
+    where
+    parseDataConstructor :: Parser ParseState (DataCon ByteString)
+    parseDataConstructor = DataCon <$> parseUpperStart <*> notAtLineStarts parseMember
 
-parseConstructor :: Parser ParseState (DataCon ByteString)
-parseConstructor = DataCon <$> parseUpperStart <*> parseWhileColumns NotLeft parseMember
+    parseMember :: Parser ParseState (Member ByteString)
+    parseMember = (MemberType <$> parseUpperStart)
+              <|> (MemberVar  <$> parseLowerStart)
 
-parseMember :: Parser ParseState (Member ByteString)
-parseMember = (MemberType <$> parseUpperStart)
-          <|> (MemberVar  <$> parseLowerStart)
-
-parseTypeVariable :: Parser ParseState (TyVar ByteString)
-parseTypeVariable = TyVar <$> parseLowerStart
+    parseTypeVariable :: Parser ParseState (TyVar ByteString)
+    parseTypeVariable = TyVar <$> parseLowerStart
