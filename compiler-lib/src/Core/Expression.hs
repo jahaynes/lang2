7fb75dc95fc12195a0e67f60b9a1d030a5731119
John Haynes <john.andrew.haynes@gmail.com>
2022-05-12 10:28:05 +0200

    playing around with recursion schemes

--- compiler-lib/src/Core/Expression.hs
+++ compiler-lib/src/Core/Expression.hs
@@ -3,19 +3,49 @@
 module Core.Expression where
 
 import Core.Operator (BinOp, UnOp)
-import Core.Term     (Term)
+import Core.Term     -- (Term)
 
-              -- Visible in source code
-data Expr s = ETerm (Term s)
-            | ELam [s] (Expr s)
-            | EApp (Expr s) [Expr s]
-            | ELet s (Expr s) (Expr s)
-            | EUnPrimOp UnOp (Expr s)
-            | EBinPrimOp BinOp (Expr s) (Expr s)
-            | IfThenElse (Expr s) (Expr s) (Expr s)
 
-              -- Generated during compilation
-            | EClo [s] [s] (Expr s)
-            | CallClo s [s]
+type Expr s = RTerm (ExprF s)
 
-                deriving (Eq, Functor, Show)
+
+-- With recursion schemes
+
+               -- Visible in source code
+data ExprF s e = FTerm (Term s)
+               | FLam [s] e
+               | FApp e [e]
+               | FLet s e e
+               | FUnPrimOp UnOp e
+               | FBinPrimOp BinOp e e
+               | FIfThenElse e e e
+
+               -- Generated during compilation
+               | FClo [s] [s] e
+               | FCallClo s [s]
+                   deriving (Eq, Functor, Show)
+
+newtype RTerm f = In { out :: f (RTerm f) }
+
+
+topDown, bottomUp :: Functor f => (RTerm f -> RTerm f) -> RTerm f -> RTerm f
+topDown  f =     In . fmap (topDown f)  . out . f
+bottomUp f = f . In . fmap (bottomUp f) . out
+
+
+
+topDown1 :: Functor f => (a -> RTerm f -> RTerm f) -> RTerm f -> RTerm f
+topDown1 f = In . fmap (topDown f)  . out . f
+
+
+
+flattenTerm :: Expr s -> Expr s
+flattenTerm (In (FLam _ e )) = e  -- remove all Parens
+flattenTerm other = other       -- do nothing otherwise
+
+flatten'' :: Expr s -> Expr s
+flatten'' = bottomUp flattenTerm
+
+
+scopy :: Expr s -> Expr s
+scopy = topDown1 (\z e -> e)
--- compiler-lib/src/Core/Module.hs
+++ compiler-lib/src/Core/Module.hs
@@ -7,20 +7,16 @@ import Core.Expression
 data Module s =
     Module { getDataDefns :: [DataDefn s]
            , getFunDefns  :: [FunDefn s]
-           } deriving (Functor, Show)
+           }
 
 data DataDefn s =
     DataDefn s [s] [DataCon s]
-        deriving (Functor, Show)
 
 data FunDefn s =
     FunDefn s (Expr s)
-        deriving (Eq, Functor, Show)
 
 data DataCon s =
     DataCon s [Member s]
-        deriving (Functor, Show)
 
 data Member s = MemberType s
               | MemberVar s
-                  deriving (Functor, Show)
