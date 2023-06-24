b2a24f5b30735ef532a7c61278030b090286f970
John Haynes <john.andrew.haynes@gmail.com>
2023-06-24 16:58:46 +0200

    wip

--- compiler-lib/src/Phase/CodeGen/CodeGenA.hs
+++ compiler-lib/src/Phase/CodeGen/CodeGenA.hs
@@ -2,12 +2,23 @@
 
 module Phase.CodeGen.CodeGenA where
 
+import Common.EitherT (EitherT (..), left)
+import Common.State
+import Common.StateT
+import Common.Trans
 import Core.Term               (Term (..))
-import Phase.Anf.AnfExpression (AExp (..), NExp (..))
+import Core.Types
+import Phase.Anf.AnfExpression (AExp (..), CExp (..), NExp (..))
 import Phase.Anf.AnfModule     (AnfModule (..), FunDefAnfT (..))
 
 import           Data.ByteString             (ByteString)
 import qualified Data.ByteString.Char8 as C8
+
+type Cg a =
+    EitherT ByteString (State Gen) a
+
+newtype Gen =
+    Gen { regCount :: Int }
 
              -- Simple
 data AInstr s = ANoOp
@@ -18,7 +29,10 @@ data AInstr s = ANoOp
               | Allocate Allocable
 
              -- Control Flow
-              | Ret Returnable
+              | Push s (Type s) SVal -- debugname / type / idunno?
+              | Pop  s (Type s) SVal -- debugname / type / val
+              | Call s
+              | Ret SVal
 
                   deriving Show
 
@@ -26,17 +40,22 @@ data Allocable =
     Allocable {- Perhaps String, DataCons, Closure -}
         deriving Show
 
-data Returnable = VirtRegPrim !Int
-                | VirtRegPtr !Int
-                | RLitInt !Integer
-                    deriving Show
+data SVal = VirtRegPrim !Int
+          | VirtRegPtr !Int
+          | RLitInt !Integer
+             deriving Show
 
 renderCodeGenA :: [AInstr ByteString]
                -> ByteString
 renderCodeGenA = C8.tail . C8.unlines . map go
     where
-    go ANoOp      = "  noop"
-    go (ALabel s) = "\n" <> s <> ":"
+    go ANoOp        = "  noop"
+    go (ALabel s)   = "\n" <> s <> ":"
+    go (AComment s) = "  // " <> s
+
+    go (Push dbgName t val) = "  push " <> go' val <> " :: " <> go'' t <> " // '" <> dbgName <> "'"
+    go (Pop  dbgName t val) = "  " <> go' val <> " <- pop :: " <> go'' t <> " // '" <> dbgName <> "'" 
+    go (Call f)   = "  call " <> f
     go (Ret r)    = "  ret " <> go' r
     go x          = "  renderCodeGenA.go: " <> C8.pack (show x)
 
@@ -44,35 +63,94 @@ renderCodeGenA = C8.tail . C8.unlines . map go
     go' (VirtRegPtr n)  = "vrp_" <> C8.pack (show n)
     go' (RLitInt i)     = C8.pack (show i)
 
+    go'' t = C8.pack (show t)
+
 codeGenModuleA :: AnfModule ByteString
                -> Either ByteString [AInstr ByteString]
-codeGenModuleA modu = concat <$> mapM codeGenFunDefn (getFunDefAnfTs modu)
+codeGenModuleA modu = 
+    let xs = concat <$> mapM codeGenFunDefn (getFunDefAnfTs modu)
+    in evalState (runEitherT xs) (Gen 0)
 
 codeGenFunDefn :: FunDefAnfT ByteString
-               -> Either ByteString [AInstr ByteString]
+               -> Cg [AInstr ByteString]
 codeGenFunDefn (FunDefAnfT name _quant nexp) = do
     (r, nexp') <- codeGenNexp nexp
-    Right $ concat [ [ALabel name]
-                   , nexp'
-                   , [Ret r] ]
+    pure $ concat [ [ALabel name]
+                  , nexp'
+                  , [Ret r] ]
+
+-- perhaps aexp and cexp can vary in how they accept/return binders?
 
 codeGenNexp :: NExp ByteString
-            -> Either ByteString (Returnable, [AInstr ByteString])
+            -> Cg (SVal, [AInstr ByteString])
 codeGenNexp (AExp aexp)  = codeGenAexp aexp
-codeGenNexp (NLet _ _ _) = Right (unkn, [AComment "nlet"])
-codeGenNexp nexp         = Left (C8.pack $ show nexp)
+codeGenNexp (CExp cexp)  = codeGenCexp cexp
+codeGenNexp (NLet _ _ _) = pure (unkn, [AComment "nlet"])
+codeGenNexp nexp         = left (C8.pack $ show nexp)
 
 codeGenAexp :: AExp ByteString
-            -> Either ByteString (Returnable, [AInstr ByteString])
+            -> Cg (SVal, [AInstr ByteString])
 codeGenAexp (ATerm _type term)          = codeGenTerm term
-codeGenAexp (ALam _type _vs _nexp)      = Right (unkn, [AComment "lam"])
-codeGenAexp (AClo _type _fvs _vs _nexp) = Right (unkn, [AComment "closure"])
-codeGenAexp aexp                        = Left (C8.pack $ show aexp)
+codeGenAexp (ALam t vs nexp)            = codegenLam t vs nexp
+codeGenAexp (AClo _type _fvs _vs _nexp) = pure (unkn, [AComment "closure"])
+codeGenAexp aexp                        = left (C8.pack $ show aexp)
+
+codegenLam t vs nexp = do
+
+    pops <- expressPop t vs
+
+    (ret, nexp') <- codeGenNexp nexp
+
+    let instrs = concat [ pops
+                        , nexp' ]
+
+    pure (ret, instrs)
+
+
+-- TODO think this one out:
+bind (TyArr a b) (v:vs) = (a,v) : bind b vs
+bind t               [] = []
+
+-- forward order
+expressPop t vs =
+    mapM go (bind t vs)
+    where
+    go (t, v) = Pop v t <$> freshRegisterFor t
+
+-- reverse order
+expressPushes =
+    mapM go . reverse
+    where
+    go (ATerm t (LitInt i)) = pure $ Push "lit" t (RLitInt i)
+
+codeGenCexp :: CExp ByteString
+            -> Cg (SVal, [AInstr ByteString])
+codeGenCexp (CApp t f xs) = codeGenApp t f xs
+
+codeGenApp t (ATerm _ (Var v)) xs = do
+    pushes <- expressPushes xs
+    let instrs = concat [ [AComment "app"]
+                        , pushes
+                        , [Call v]
+                        ]
+    pure (unkn, instrs)
+
 
 codeGenTerm :: Term ByteString
-            -> Either ByteString (Returnable, [AInstr ByteString])
-codeGenTerm (LitInt i) = Right (RLitInt i, [])
-codeGenTerm term       = Left $ "codeGenTerm: " <> C8.pack (show term)
+            -> Cg (SVal, [AInstr ByteString])
+codeGenTerm (LitInt i) = pure (RLitInt i, [])
+codeGenTerm (Var v)    = error "lookup bound register"
+codeGenTerm term       = left $ "codeGenTerm: " <> C8.pack (show term)
 
-unkn :: Returnable
-unkn = VirtRegPtr (-1)
+unkn :: SVal
+unkn = VirtRegPtr 99
+
+freshRegisterFor :: Type ByteString -> Cg SVal
+freshRegisterFor t = do
+    gen <- lift get
+    let rc = regCount gen
+    lift $ put gen { regCount = rc + 1 }
+    pure $ case t of
+        TyCon "Int"  [] -> VirtRegPrim rc
+        TyCon "Bool" [] -> VirtRegPrim rc
+        _               -> VirtRegPtr rc
