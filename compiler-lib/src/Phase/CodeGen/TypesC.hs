e820a6f83192eb9559424a2ab27de7878d76cb1e
John Haynes <john.andrew.haynes@gmail.com>
2024-03-16 16:59:06 +0100

    fleshing out interpreter

--- compiler-lib/src/Phase/CodeGen/TypesC.hs
+++ compiler-lib/src/Phase/CodeGen/TypesC.hs
@@ -1,6 +1,13 @@
 module Phase.CodeGen.TypesC where
 
-import Data.Text (Text)
+import           Control.Monad (zipWithM_)
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import           Data.Maybe (fromJust)
+import           Data.Text (Text)
+import           Data.Vector ((!), Vector)
+import qualified Data.Vector as V
+import           Debug.Trace (trace)
 
 data CInstr s = CComment !s 
               | CLabel !s
@@ -17,29 +24,110 @@ data CInstr s = CComment !s
               | CTimes !Int !(CVal s) !(CVal s) -- dest reg / a / b 
 
               | CMov !(MovMode s)
+                  deriving Show
 
 data CallDest s = CallLabel !s
                 | CallReg !Int
-                | CallClosureAddr !Int
+                | CallClosureAddr !Int  -- reg containing ptr to closure
+                    deriving Show
 
 data CVal s = CLitInt !Int
             | CReg !Int
             | CLbl !s
+                deriving Show
 
 data MovMode s = MovMode
-               | ToFrom !Int !(CVal s)
                | ToOffsetFrom !Int !Int !(CVal s)
                | ToFromOffset !Int !Int !Int
+                   deriving Show
 
 typesCMain :: IO ()
 typesCMain = do
 
+    mapM_ print $ zip [0..] allInstrs
+    putStrLn ""
 
-    putStrLn "hu"
+    interpret allInstrs
+
+
+
+interpret :: [CInstr Text] -> IO ()
+interpret is = do
+    
+    let iv = V.fromList is
+
+    let loop !ram !free !valStack !ipStack !regs !ip =
+            let i = iv ! ip in
+            case i of
+
+                CLabel{} ->
+                    loop ram free valStack ipStack regs (ip+1)
+
+                CPush (CLitInt i) ->
+                    let valStack' = i:valStack
+                    in loop ram free valStack' ipStack regs (ip+1)
+
+                CCall (CallLabel f) ->
+                    let ipStack' = ip+1:ipStack
+                    in loop ram free ipStack' valStack regs (loc iv f)
+
+                CPop r ->
+                    let (v:valStack') = valStack
+                        regs' = M.insert r v regs
+                    in loop ram free ipStack valStack' regs' (ip+1)
+
+                CTimes d a b ->
+                    let Just a' = case a of
+                                      CReg r    -> M.lookup r regs
+                                      CLitInt i -> Just i
+                        Just b' = case b of
+                                      CReg r    -> M.lookup r regs
+                                      CLitInt i -> Just i
+                        regs' = M.insert d (a' * b') regs
+                    in loop ram free ipStack valStack regs' (ip+1)
+
+                CAlloc r sz ->
+                    let (cells, 0) = sz `divMod` 8
+                        free' = free + cells
+                        regs' = M.insert r free regs
+                    in loop ram free' ipStack valStack regs' (ip+1)
+
+                CMov (ToOffsetFrom d o (CLbl f)) ->
+                    let Just p    = M.lookup d regs
+                        (cell, 0) = (p + o) `divMod` 8
+                        ram' = M.insert cell (loc iv f) ram
+                    in loop ram' free ipStack valStack regs (ip+1)
+
+                CMov (ToOffsetFrom d o (CReg r)) ->
+                    let Just v    = M.lookup r regs
+                        Just p    = M.lookup d regs
+                        (cell, 0) = (p + o) `divMod` 8
+                        ram' = M.insert cell v ram
+                    in loop ram' free ipStack valStack regs (ip+1)
+    
+                CRet (CReg 5) ->
+                    let Just v     = M.lookup 5 regs
+                        ip':stack' = ipStack
+                        valStack'  = v:valStack
+                    in loop ram free stack' valStack' regs ip'
+
+                _        -> error $ "Unhandled " ++ show i
+
+    loop M.empty 0 [-1] [] M.empty 0
+
+    where
+    loc iv f = fromJust $ V.findIndex isLabel iv
+        where
+        isLabel (CLabel l) = l == f
+        isLabel          _ = False
+
+allInstrs = concat [mainIs, fIs, anf0Is]
+
 
 
 mainIs :: [CInstr Text]
-mainIs = [ CPush (CLitInt 1)
+mainIs = [ CLabel "main"
+         , CPush (CLitInt 1)
          , CCall (CallLabel "f")
          , CPop 1 -- reg a
          , CPush (CLitInt 2)
@@ -49,16 +137,18 @@ mainIs = [ CPush (CLitInt 1)
          ]
 
 fIs :: [CInstr Text]
-fIs = [ CPop 3 {- r3 <- pop x -}
+fIs = [ CLabel "f"
+      , CPop 3 {- r3 <- pop x -}
       , CTimes 4 (CReg 3) (CReg 3) {- r4 <- r3 * r3 -}
       , CAlloc 5 16
-      , CMov (ToFrom 5 (CLbl "anf_0"))
+      , CMov (ToOffsetFrom 5 0 (CLbl "anf_0"))
       , CMov (ToOffsetFrom 5 8 (CReg 4))
       , CRet (CReg 5)
       ]
 
-anf0Is :: [CInstr s]
-anf0Is = [ CPop 6 {-pop &env-}
+anf0Is :: [CInstr Text]
+anf0Is = [ CLabel "anf_0"
+         , CPop 6 {-pop &env-}
          , CMov (ToFromOffset 7 6 8) {- xx = r6[+8] -}
          , CPop 8 {-pop y-}
          , CPlus 9 (CReg 8) (CReg 7)
