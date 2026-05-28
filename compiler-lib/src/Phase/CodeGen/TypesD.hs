1b5f5337ef8f3d1a673a63b522541b22abd2ea58
John Haynes <john.andrew.haynes@gmail.com>
2026-05-28 12:28:04 +0200

    wip pretty-printing

--- compiler-lib/src/Phase/CodeGen/TypesD.hs
+++ compiler-lib/src/Phase/CodeGen/TypesD.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Phase.CodeGen.TypesD ( CallDest (..)
                             , DBinOp (..)
                             , DInstr (..)
@@ -5,6 +7,8 @@ module Phase.CodeGen.TypesD ( CallDest (..)
                             , MovMode (..)
                             , R (R)
                             ) where
+
+import Data.String.Interpolate (i)
 
 newtype R =
     R Int
@@ -22,7 +26,7 @@ data DInstr s = DComment !s
 
               | DCall !(CallDest s)
                 
-              | DFun ![DVal s] [DInstr s]
+          --    | DFun ![DVal s] [DInstr s]
 
               | DRet !(DVal s)
 
@@ -33,13 +37,46 @@ data DInstr s = DComment !s
 
               | DCmpB !R
               | J !s
-              | Je !s
+         --     | Je !s
               | Jne !s
 
               | DMov !(MovMode s)
               | DErr !s
 
-                  deriving Show
+--                  deriving Show
+
+-- Hacky newtype for stripping excess quotes
+newtype Uq s =
+    Uq s
+
+instance Show s => Show (Uq s) where
+    show (Uq s) = init . tail . show $ s
+
+instance Show s => Show (DInstr s) where
+
+    show (DComment c) = [i|// #{c}|]
+
+    show (DLabel l) = [i|#{Uq l}:|]
+
+    show (DPush v) = [i|push #{v}|]
+
+    show (DPop r) = [i|#{r} <- pop|]
+
+    show (DCall dst) = [i|call #{dst}|]
+
+    show (DRet v) = [i|ret #{v}|]
+
+    show (DBin dst op a b) = [i|#{dst} <- #{a} #{op} #{b}|]
+
+    show (DNeg r) = [i|-#{r}|]
+
+    show (DCmpB r) = [i|cmp #{r}|]
+
+    show (J l) = [i|jmp #{l}|]
+
+    show (Jne l) = [i|jmpNe #{l}|]
+
+    show (DMov mode) = show mode
 
 data DBinOp = DPlus
             | DMinus
@@ -50,7 +87,19 @@ data DBinOp = DPlus
             | DAnd
             | DOr
             | DLt
-                deriving Show
+
+instance Show DBinOp where
+    show op =
+        case op of
+            DPlus  -> "+"
+            DMinus -> "-"
+            DTimes -> "*"
+            DDiv   -> "/"
+            DMod   -> "%"
+            DEq    -> "=="
+            DAnd   -> "&&"
+            DOr    -> "||"
+            DLt    -> "<"
 
 data CallDest s = CallLabel !s
                 | CallReg !R                                      
@@ -59,7 +108,11 @@ data CallDest s = CallLabel !s
 data DVal s = DLitInt !Int
             | DReg !R
             | DLbl !s
-                deriving Show
+
+instance Show s => Show (DVal s) where
+    show (DLitInt i) = show i
+    show (DReg r)    = show r
+    show (DLbl s)    = show (Uq s)
 
 data MovMode s = ToFrom !R !R
                | FromLitInt !R !Int
