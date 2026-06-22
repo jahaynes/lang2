7a98e1d78c3adb6f875106ca455828e7b6ea7ebd
John Haynes <john.andrew.haynes@gmail.com>
2026-06-22 21:06:50 +0200

    printy

--- compiler-explorer-lib/src/Pretty/Direct.hs
+++ compiler-explorer-lib/src/Pretty/Direct.hs
@@ -1,10 +1,39 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings,
+             QuasiQuotes #-}
 
 module Pretty.Direct where
 
 import Phase.CodeGen.Direct
 
-import Data.Text (Text)
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           Data.String.Interpolate (i)
 
 renderCodeGen :: Code -> Text
-renderCodeGen code = "TO RENDER"
+renderCodeGen (Code fs asm) = do
+    
+    --let fs' = unlines . map show $ fs
+    
+    --let asm' = unlines . map show $ asm
+
+    -- pack $ fs' ++ asm'
+
+    T.unlines . concat $ [ map renderFunc fs
+                         , map renderAsm asm
+                         ]
+
+renderFunc :: Func -> Text
+renderFunc (Func name vs body) =
+    [i|#{name}(#{vs}(#{T.unlines $ map renderAsm body}))|]
+
+renderAsm :: Asm -> Text
+renderAsm = T.pack . show
+
+{-
+
+-- | A compiled top-level function: its name, formal parameters and body.
+data Func = Func ByteString [ByteString] [Asm]
+    deriving (Eq, Show)
+
+
+-}
