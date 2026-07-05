ec7710f9f618bcd955d20ce706aec075bc2389e0
John Haynes <john.andrew.haynes@gmail.com>
2026-07-05 11:20:38 +0200

    switch to set

--- compiler-lib/src/Phase/Anf/FreeVars.hs
+++ compiler-lib/src/Phase/Anf/FreeVars.hs
@@ -1,55 +1,43 @@
 module Phase.Anf.FreeVars where
 
-import           Data.Set ((\\), Set, fromList, toList, union, unions)
+import           Data.Set ((\\), Set, fromList, union, unions)
 import qualified Data.Set as S
 
 import Core.Term     (Term (..))
 import Phase.Anf.Anf
 
 class FreeVars f where
-    fvs :: Ord v => f v -> [v]
+    fvs :: Ord v => f v -> Set v
 
-termFvs :: Term v -> [v]
-termFvs (Var v) = [v]
-termFvs _       = []
+termFvs :: Term v -> Set v
+termFvs (Var v) = S.singleton v
+termFvs _       = S.empty
 
-boundVarsOf :: PPat v -> [v]
-boundVarsOf (PVar v)      = [v]
-boundVarsOf (PApp _ _ ts) = [v | Var v <- ts]   -- TODO check this
+boundVarsOf :: Ord v => PPat v -> Set v
+boundVarsOf (PVar v)      = S.singleton v
+boundVarsOf (PApp _ _ ts) = fromList [v | Var v <- ts]   -- TODO check this
 
 instance FreeVars AExp where
     fvs (ATerm _ t) = termFvs t
 
 instance FreeVars CExp where
     fvs (CUnPrimOp _ _ a)     = fvs a
-    fvs (CBinPrimOp _ _ a b)  = toList
-                              $ fromList (fvs a) `union` fromList (fvs b)
-    fvs (CIfThenElse _ p a b) = toList
-                              $ fromList (fvs p) `union` fromList (fvs a)
-                                                 `union` fromList (fvs b)
-    fvs (CApp _ f xs)         = toList
-                              $ fromList (fvs f) `union`
-                                  unions (map (fromList . fvs) xs)
-    fvs (CAppClo _ f e xs)    = toList
-                              $ fromList (fvs f) `union` fromList (fvs e)
-                                                 `union`
-                                  unions (map (fromList . fvs) xs)
-    fvs (CCase _ s ps)        = toList
-                              $ fromList (fvs s) `union`
-                                  unions (map (fromList . fvs) ps)
+    fvs (CBinPrimOp _ _ a b)  = fvs a `union` fvs b
+    fvs (CIfThenElse _ p a b) = fvs p `union` fvs a `union` fvs b
+    fvs (CApp _ f xs)         = fvs f `union` unions (map fvs xs)
+    fvs (CAppClo _ f e xs)    = fvs f `union` fvs e `union` unions (map fvs xs)
+    fvs (CCase _ s ps)        = fvs s `union` unions (map fvs ps)
 
 instance FreeVars NExp where
     fvs (AExp a)        = fvs a
     fvs (CExp c)        = fvs c
-    fvs (NLet _ v b c)  = toList
-                        $ fromList (fvs b) `union` S.delete v (fromList (fvs c))
+    fvs (NLet _ v b c)  = fvs b `union` S.delete v (fvs c)
 
 instance FreeVars PExp where
-    fvs (PExp pat body) = toList
-                        $ fromList (fvs body) \\ fromList (boundVarsOf pat)
+    fvs (PExp pat body) = fvs body \\ boundVarsOf pat
 
 instance FreeVars PPat where
-    fvs _ = []
+    fvs _ = S.empty
 
 instance FreeVars AClosEnv where
-    fvs (AClosEnv vs) = vs
+    fvs (AClosEnv vs) = fromList vs
